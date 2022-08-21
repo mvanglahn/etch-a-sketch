28dc9190345f676875c7c3a6dcbd9a5159a51a56
Michael Van Glahn <michael.vanglahn.jr@gmail.com>
2022-08-21 17:29:54 -0400

    Working JS createGrid

--- javascript.js
+++ javascript.js
@@ -1,12 +1,17 @@
 function createGrid(rows, cols){
     for(let i=0; i<rows; i++){
+        const grid = document.getElementById('grid-frame')
         const createRow = document.createElement('div');
-        createRow.classList.add('row', 'grid');
+        createRow.className = 'row', 'grid';
+        
         for(let j=0; j<cols; j++){
             const createCol = document.createElement('div');
-            createCol.classList.add('cols', 'grid');
+            const currentRow = document
+            createCol.className = 'cols', 'grid';
             createRow.appendChild(createCol);
         }
+        grid.appendChild(createRow);
     }
+    document.body.appendChild(document.getElementById('grid-frame'));
 }
 createGrid(16,16);
