2bd559d3a54a400751c30f0b9601d4ee76246051
Michael Van Glahn <michael.vanglahn.jr@gmail.com>
2022-08-22 13:24:23 -0400

    Using dynamic CSS grid

--- javascript.js
+++ javascript.js
@@ -1,17 +1,49 @@
-function createGrid(rows, cols){
-    for(let i=0; i<rows; i++){
-        const grid = document.getElementById('grid-frame')
-        const createRow = document.createElement('div');
-        createRow.className = 'row', 'grid';
-        
-        for(let j=0; j<cols; j++){
-            const createCol = document.createElement('div');
-            const currentRow = document
-            createCol.className = 'cols', 'grid';
-            createRow.appendChild(createCol);
-        }
-        grid.appendChild(createRow);
+const sketchArea = document.querySelector('.etchASketch');
+const gridOut = createEle(sketchArea,'div',' ','grid-frame')
+const sketch = {x:16, y:16};
+
+createGrid();
+
+function createGrid(){
+    const total = sketch.x * sketch.y;
+    for (let i=0; i<total; i++){
+        const temp = createEle(gridOut,'div', `${i+1}`,'grid');
     }
-    document.body.appendChild(document.getElementById('grid-frame'));
+    gridOut.style.setProperty(`grid-template-columns`, `repeat(${sketch.y},1fr)`)
 }
-createGrid(16,16);
+    
+
+function createEle(parent, eleType, html, eleClass){
+    const ele = document.createElement(eleType);
+    ele.innerHTML = html;
+    ele.classList.add(eleClass);
+    return parent.appendChild(ele);
+}
+
+/*function createGrid(col, row){
+    for(let i=0; i<col; i++){
+        const grid = document.getElementById('grid-frame')
+        const createCol = document.createElement('div');
+        createCol.className = 'grid';
+        
+        for(let j=0; j<row; j++){
+            const createRow = document.createElement('div');
+            createRow.className = 'grid';
+            createCol.appendChild(createRow);
+            document.body.appendChild(document.getElementById('grid-frame'));
+        }
+        grid.appendChild(createCol);
+        
+    }
+    
+}
+//createGrid(16,16);
+
+function gridCreate (col, row){
+    const grid = document.getElementById('grid-frame');
+    grid.style.gridTemplateColumns = `repeat(${col}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${row}, 1fr)`
+    
+}
+
+gridCreate(16,16);*/
