934f1abcceb93d7a7938ef03429f207b8e81f49e
Michael Van Glahn <michael.vanglahn.jr@gmail.com>
2022-08-23 18:20:53 -0400

    Resize button added

--- javascript.js
+++ javascript.js
@@ -15,7 +15,7 @@ function createGrid(){
 
 function createEle(parent, eleType, html, eleClass){
     const ele = document.createElement(eleType);
-    ele.innerHTML = html;
+    //ele.innerHTML = html;
     ele.classList.add(eleClass);
     return parent.appendChild(ele);
 }
@@ -31,3 +31,22 @@ function hoverOn(){
 function hoverOff(){
     box.setAttribute('style', 'background-color: white')
 }
+
+const resizeBtn = document.getElementById('resize');
+resizeBtn.addEventListener('click', resizeGrid);
+
+function resizeGrid(){
+    let newSize = prompt("What size grid would you like?", "0");
+    let newSizeInt = parseInt(newSize, 10);
+    if (0 < newSizeInt < 100){
+        clearGrid();
+        createGrid(newSizeInt, newSizeInt);
+    } else {
+        alert('Please enter a number between 0 and 100.')
+    }
+}
+
+function clearGrid(){
+    document.querySelectorAll(".grid").forEach(el => el.remove());
+
+    }
