97b88c2796226cabf4078f87928b9863f8be4679
Michael Van Glahn <michael.vanglahn.jr@gmail.com>
2022-08-24 13:05:00 -0400

    Removed sketch, fixed resize & hoverOn

--- javascript.js
+++ javascript.js
@@ -1,15 +1,16 @@
 const sketchArea = document.querySelector('.etchASketch');
 const gridOut = createEle(sketchArea,'div',' ','grid-frame')
-const sketch = {x:16, y:16};
+//const sketch = {x:16, y:16};
 
-createGrid();
+createGrid(16*16);
 
-function createGrid(){
-    const total = sketch.x * sketch.y;
+function createGrid(total){
     for (let i=0; i<total; i++){
         const temp = createEle(gridOut,'div', `${i+1}`,'grid');
     }
-    gridOut.style.setProperty(`grid-template-columns`, `repeat(${sketch.y},1fr)`)
+    gridOut.style.setProperty(`grid-template-columns`, `repeat(${Math.sqrt(total)},1fr)`)
+    var box = document.querySelectorAll('.grid');
+    box.forEach(elem => elem.addEventListener('mouseover', hoverOn, false));
 }
     
 
@@ -18,10 +19,10 @@ function createEle(parent, eleType, html, eleClass){
     //ele.innerHTML = html;
     ele.classList.add(eleClass);
     return parent.appendChild(ele);
+    
 }
 
-var box = document.querySelectorAll('.grid');
-box.forEach(elem => elem.addEventListener('mouseover', hoverOn, false));
+
 //box.addEventListener('mouseovut', hoverOff, false);
 
 function hoverOn(){
@@ -40,10 +41,11 @@ function resizeGrid(){
     let newSizeInt = parseInt(newSize, 10);
     if (0 < newSizeInt < 100){
         clearGrid();
-        createGrid(newSizeInt, newSizeInt);
+        createGrid(newSizeInt*newSizeInt);
     } else {
         alert('Please enter a number between 0 and 100.')
     }
+
 }
 
 function clearGrid(){
